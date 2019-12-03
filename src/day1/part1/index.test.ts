54e154aeecc005692fb0a01bb4f2e4bbf0f83b12
Maarten Ackermans <maarten.ackermans@gmail.com>
2019-12-03 12:35:38 +0100

    feat(day1): add example tests for part 1

--- src/day1/part1/index.test.ts
+++ src/day1/part1/index.test.ts
@@ -6,6 +6,34 @@ import solution from '.'
 
 describe('Day 1', () => {
   describe('Part 1', () => {
+    it('example mass 12', () => {
+      const result = solution([
+        12,
+      ])
+      expect(result).toBe(2)
+    })
+
+    it('example mass 14', () => {
+      const result = solution([
+        14,
+      ])
+      expect(result).toBe(2)
+    })
+
+    it('example mass 1969', () => {
+      const result = solution([
+        1969,
+      ])
+      expect(result).toBe(654)
+    })
+
+    it('example mass 100756', () => {
+      const result = solution([
+        100756,
+      ])
+      expect(result).toBe(33583)
+    })
+
     it('calculates the sum of the fuel amount needed for each module', async () => {
       let input: Array<number> = []
 
