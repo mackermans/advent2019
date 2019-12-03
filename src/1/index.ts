f528c8afadd0b6273e4315004df681d11262c184
Maarten Ackermans <maarten.ackermans@gmail.com>
2019-12-03 11:56:50 +0100

    feat(day 1): add solution

---
+++ src/1/index.ts
@@ -0,0 +1,9 @@
+const solution = (input: Array<number>) => {
+  const fuelSum = input.reduce((sum, value) => {
+    const moduleFuelRequired = Math.floor(value / 3) - 2
+    return sum + moduleFuelRequired
+  }, 0)
+  return fuelSum
+}
+
+export default solution
