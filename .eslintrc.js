267f7097fa6e250e57cdbe1d5efd0eef46001cca
Maarten Ackermans <maarten.ackermans@gmail.com>
2019-12-03 11:55:26 +0100

    build: initial setup with typescript, babel, jest and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'quote-props': ['error', 'as-needed'],
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  }
+};
